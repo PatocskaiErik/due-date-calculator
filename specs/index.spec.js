e4754b5e7b3ccad3ecf8cd363da3eed645044465
Erik <patoerik93@gmail.com>
2022-04-08 18:02:05 +0200

    test for working hours

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -1,4 +1,5 @@
 const assert = require("assert");
+const { describe, it } = require("mocha");
 const index = require("../index");
 
 const notWeekend = new Date(Date.UTC(2022, 4, 5, 15, 0));
@@ -14,6 +15,18 @@ describe("Date is on weekend", () => {
 
   it("should return false", () => {
     const result = index.isWeekend(notWeekend);
+    assert.strictEqual(result, false);
+  });
+});
+
+describe("Date is out of working hours", () => {
+  it("should return true", () => {
+    const result = index.isOutOfWorkingHours(outOfWorkingHours);
+    assert.strictEqual(result, true);
+  });
+
+  it("should return false", () => {
+    const result = index.isOutOfWorkingHours(inWorkingHours);
     assert.strictEqual(result, false);
   });
 });
